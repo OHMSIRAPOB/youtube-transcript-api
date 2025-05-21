68f7827d0da7a8b0b93e5b283b2d631aa4e1f674
Siraphob Lueangprasert <110965591+OHMSIRAPOB@users.noreply.github.com>
2025-05-22 02:10:09 +0700

    Update main.py
    แก้

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api._api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import JSONFormatter
 
 app = Flask(__name__)
